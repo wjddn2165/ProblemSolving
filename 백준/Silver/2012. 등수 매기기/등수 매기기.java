0389ea2144553c4d941c27e3932aa3e2cebbffd6
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2023-04-07 00:39:16 +0900

    [Silver III] Title: 등수 매기기, Time: 644 ms, Memory: 56640 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
+        }
+
+        long result = 0;
+
+        for (int i = 1; i <= N; i++) {
+            result += Math.abs(i - pq.remove());
+        }
+
+        System.out.println(result);
+    }
+}
