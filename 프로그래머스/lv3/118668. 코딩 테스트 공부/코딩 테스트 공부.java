dcc644f40750e73d40c8009a60ddc320c71dd08a
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2023-04-30 22:08:05 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 1.97 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -3,10 +3,38 @@ import java.util.*;
 class Solution {
     final int INF = 987654321;
     
-    public int solution(int alp, int cop, int[][] problems) {
-        int maxAlp = 0;
-        int maxCop = 0;
+    int maxAlp = 0;
+    int maxCop = 0;
+    
+    int[][] dp;
+    
+    int dfs(int alp, int cop, List<int[]> list) {
+        if(alp == maxAlp && cop == maxCop) {
+            return 0;
+        }
         
+        if(dp[alp][cop] != INF) {
+            return dp[alp][cop];
+        }
+        
+        dp[alp][cop] = INF + 1;
+                
+        for(int i=0;i<list.size();i++) {
+            int[] problem = list.get(i);
+            
+            if(alp >= problem[0] && cop >= problem[1]) {
+                int nextAlp = Math.min(alp + problem[2], maxAlp);
+                int nextCop = Math.min(cop + problem[3], maxCop);
+                
+                dp[alp][cop] = Math.min(dp[alp][cop],
+                                    dfs(nextAlp, nextCop, list) + problem[4]);
+            }
+        }
+        
+        return dp[alp][cop];
+    }
+    
+    public int solution(int alp, int cop, int[][] problems) {
         List<int[]> list = new ArrayList<>();
         
         for(int i=0;i<problems.length;i++) {
@@ -22,7 +50,7 @@ class Solution {
         list.add(new int[]{0, 0, 1, 0, 1});
         list.add(new int[]{0, 0, 0, 1, 1});
         
-        int[][] dp = new int[maxAlp + 1][maxCop + 1];
+        dp = new int[maxAlp + 1][maxCop + 1];
         
         for(int i=alp;i<=maxAlp;i++) {
             for(int j=cop;j<=maxCop;j++) {
@@ -30,29 +58,6 @@ class Solution {
             }
         }
         
-        dp[alp][cop] = 0;
-        
-        for(int i=alp;i<=maxAlp;i++) {
-            for(int j=cop;j<=maxCop;j++) {
-                
-                if(i == maxAlp && j == maxCop) {
-                    return dp[i][j];
-                }
-                
-                for(int k=0;k<list.size();k++) {
-                    int[] problem = list.get(k);
-                    
-                    if(problem[0] <= i && problem[1] <= j) {
-                        
-                        int nextAlp = Math.min(i + problem[2], maxAlp);
-                        int nextCop = Math.min(j + problem[3], maxCop);
-                        
-                        dp[nextAlp][nextCop] = Math.min(dp[nextAlp][nextCop], dp[i][j] + problem[4]);
-                    }
-                }
-            }
-        }
-        
-        return dp[maxAlp][maxCop];
+        return dfs(alp, cop, list);
     }
 }
