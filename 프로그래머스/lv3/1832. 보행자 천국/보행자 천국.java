011097dba93d37a12cc7fc6be8291940e0587ded
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2023-06-10 00:52:07 +0900

    [level 3] Title: 보행자 천국, Time: 188.32 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/1832. 보행자 천국/보행자 천국.java
@@ -0,0 +1,55 @@
+class Solution {
+    int MOD = 20170805;
+    int[][][] dp;
+    int[] dr = {-1, 0, 1, 0};
+    int[] dc = {0, 1, 0, -1};
+    int m;
+    int n;
+    
+    public int solution(int m, int n, int[][] cityMap) {
+        this.m = m;
+        this.n = n;
+        
+        dp = new int[m][n][4];
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                for(int k=0;k<4;k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        
+        return dfs(0, 0, 1, cityMap);
+    }
+    
+    int dfs(int r, int c, int dir, int[][] cityMap) {
+        if(r < 0 || c < 0 || r >= m || c >= n) {
+            return 0;
+        }
+        
+        if(r == m - 1 && c == n - 1) {
+            return 1;
+        }
+        
+        if(dp[r][c][dir] != -1) {
+            return dp[r][c][dir];
+        }
+        
+        dp[r][c][dir] = 0;
+        
+        if(cityMap[r][c] == 0) {
+            dp[r][c][dir] += dfs(r, c + 1, 1, cityMap) % MOD;
+            dp[r][c][dir] += dfs(r + 1, c, 2, cityMap) % MOD;
+        } else if(cityMap[r][c] == 2) {
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
+            
+            dp[r][c][dir] += dfs(nr, nc, dir, cityMap) % MOD;
+        } else {
+            return 0;
+        }
+        
+        return dp[r][c][dir] % MOD;
+    }
+}
