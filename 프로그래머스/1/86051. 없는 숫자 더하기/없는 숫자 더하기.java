e595c2a38d629cee5cb38099cc7a3e2ee1cb4e17
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2023-10-25 00:18:33 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.77 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,21 +1,7 @@
 import java.util.stream.*;
-import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<numbers.length;i++) {
-            set.add(numbers[i]);
-        }
-        
-        int sum = 0;
-        
-        for(int i=0;i<10;i++) {
-            if(!set.contains(i)) {
-                sum += i;
-            }
-        }
-        
-        return sum;
+        return 45 - IntStream.of(numbers).sum();
     }
 }
