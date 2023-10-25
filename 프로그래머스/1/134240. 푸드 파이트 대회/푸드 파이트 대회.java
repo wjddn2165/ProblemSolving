1bc7f058a92e453275aa4aa488ce2c292e67e8a6
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2023-10-25 22:07:18 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.18 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<food.length;i++) {
+            for(int j=0;j<food[i]/2;j++) {
+                sb.append(i);
+            }
+        }
+        
+        System.out.println(sb.toString());
+        
+        return new StringBuilder(sb).append("0").append(sb.reverse()).toString();
+    }
+}
