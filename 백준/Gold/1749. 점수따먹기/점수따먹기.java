70f6760233df5e273e85c53941b242fc44dd36e8
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2023-11-08 14:57:46 +0900

    [Gold IV] Title: 점수따먹기, Time: 304 ms, Memory: 35008 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] matrix = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 2; j <= m; j++) {
+                matrix[i][j] += matrix[i][j-1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = i; j <= m; j++) {
+                int[] dp = new int[n + 1];
+                dp[0] = -987654321;
+                for (int k = 1; k <= n; k++) {
+                    int next = matrix[k][j] - matrix[k][i-1];
+                    dp[k] = Math.max(dp[k-1] + next, next);
+                    max = Math.max(max, dp[k]);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
