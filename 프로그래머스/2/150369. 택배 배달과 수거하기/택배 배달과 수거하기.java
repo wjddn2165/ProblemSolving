c03ee35dc03f4c8a0ef2dcc5a214e01b6f462090
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2023-11-23 14:49:09 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 4.65 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int delivery = 0;
+        int pickup = 0;
+        for(int i=n-1;i>=0;i--) {
+            delivery += deliveries[i];
+            pickup += pickups[i];
+            
+            while(delivery > 0 || pickup > 0) {
+                answer += 2 * (i + 1);
+                delivery -= cap;
+                pickup -= cap;
+            }
+        }
+        return answer;
+    }
+}
