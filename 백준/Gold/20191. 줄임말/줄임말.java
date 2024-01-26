24f3ddf65c846c10cce182c579e80751800c2ce7
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2024-01-27 00:04:32 +0900

    [Gold II] Title: 줄임말, Time: 308 ms, Memory: 24320 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/20191. 줄임말/줄임말.java
+++ 백준/Gold/20191. 줄임말/줄임말.java
@@ -5,58 +5,59 @@ class Main {
 
     static String S;
     static String T;
+    static List<List<Integer>> pos = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         S = br.readLine();
         T = br.readLine();
 
-        Set<Character> set = new HashSet<>();
-        for (int i = 0; i < T.length(); i++) {
-            set.add(T.charAt(i));
+        for (int i = 0; i < 26; i++) {
+            pos.add(new ArrayList<>());
         }
 
+        for (int i = 0; i < T.length(); i++) {
+            char ch = T.charAt(i);
+            pos.get(ch - 'a').add(i);
+        }
+
+        int idx = -1;
+        int ans = 1;
+
         for (int i = 0; i < S.length(); i++) {
-            if (!set.contains(S.charAt(i))) {
+            char ch = S.charAt(i);
+            List<Integer> nextPos = pos.get(ch - 'a');
+            if (nextPos.size() == 0) {
                 System.out.println(-1);
                 return;
             }
+
+            int lowerBound = lowerBound(idx, nextPos);
+            if (lowerBound <= idx) {
+                ans++;
+                idx = nextPos.get(0);
+            } else {
+                idx = lowerBound;
+            }
         }
 
+        System.out.println(ans);
+    }
+
+    static int lowerBound(int idx, List<Integer> nextPos) {
         int lo = 0;
-        int hi = 1000000;
+        int hi = nextPos.size() - 1;
 
         while (lo <= hi) {
             int mid = (lo + hi) >> 1;
-            if (decide(mid)) {
+            int midValue = nextPos.get(mid);
+            if (midValue > idx) {
                 hi = mid - 1;
             } else {
                 lo = mid + 1;
             }
         }
 
-        System.out.println(lo);
-    }
-
-    static boolean decide(int n) {
-        long patternSize = (long) T.length() * n;
-        if (patternSize < S.length()) {
-            return false;
-        }
-
-        int idx = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < T.length(); j++) {
-                if (S.charAt(idx) == T.charAt(j)) {
-                    idx++;
-                    if (idx == S.length()) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
+        return nextPos.get(Math.min(lo, nextPos.size() - 1));
     }
 }
