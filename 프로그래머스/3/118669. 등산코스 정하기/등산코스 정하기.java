3c18a3f815cddc6f30d536fc543b2848f427a55d
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2024-01-28 02:00:54 +0900

    [level 3] Title: 등산코스 정하기, Time: 142.58 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,91 +1,89 @@
 import java.util.*;
 
 class Solution {
+    
+    final int INF = 987654321;
+    
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] answer = {};
+        Set<Integer> gateSet = new HashSet<>();
+        Set<Integer> summitSet = new HashSet<>();
         
-        List<List<Path>> graph = new ArrayList<>();
-        
+        List<List<Node>> graph = new ArrayList<>();
         for(int i=0;i<=n;i++) {
             graph.add(new ArrayList<>());
         }
         
-        // 그래프 관계 설정
         for(int[] path : paths) {
             int from = path[0];
             int to = path[1];
             int weight = path[2];
             
-            graph.get(from).add(new Path(to, weight));
-            graph.get(to).add(new Path(from, weight));
+            graph.get(from).add(new Node(to, weight));
+            graph.get(to).add(new Node(from, weight));
         }
         
-        Set<Integer> summitSet = new HashSet<>();
-        
-        for(int summit : summits) {
-            summitSet.add(summit);
+        for(int i=0;i<gates.length;i++) {
+            gateSet.add(gates[i]);
         }
         
-        PriorityQueue<Node> queue = new PriorityQueue<>();
+        for(int i=0;i<summits.length;i++) {
+            summitSet.add(summits[i]);
+        }
         
         int[] dist = new int[n + 1];
+        Arrays.fill(dist, INF);
+        PriorityQueue<Node> queue = new PriorityQueue<>();
         
-        for(int gate : gates) {
+        for(int gate : gateSet) {
             queue.offer(new Node(gate, 0));
-            // dist[gate] = -1;
+            dist[gate] = 0;
         }
-        
-        int minIntensity = Integer.MAX_VALUE;
-        int minSummit = Integer.MAX_VALUE;
         
         while(!queue.isEmpty()) {
             Node cur = queue.remove();
             
-            if(cur.intensity > minIntensity) {
-                break;
+            if(cur.weight > dist[cur.num]) {
+                continue;
             }
             
             if(summitSet.contains(cur.num)) {
-                minIntensity = cur.intensity;
-                minSummit = Math.min(minSummit, cur.num);
                 continue;
             }
-                        
-            for(Path path : graph.get(cur.num)) {
-                int nextIntensity = Math.max(cur.intensity, path.weight);
-                
-                if(dist[path.num] == 0 || dist[path.num] > nextIntensity) {
-                    queue.offer(new Node(path.num, nextIntensity));
-                    dist[path.num] = nextIntensity;
+            
+            for(Node next : graph.get(cur.num)) {
+                if(gateSet.contains(next.num)) continue;
+                int nextWeight = Math.max(cur.weight, next.weight);
+                if(dist[next.num] > nextWeight) {
+                    dist[next.num] = nextWeight;
+                    queue.offer(new Node(next.num, nextWeight));
                 }
             }
         }
         
-        return new int[]{minSummit, minIntensity};
+        int[] answer = {-1, INF};
+        
+        Arrays.sort(summits);
+        
+        for(int summit : summits) {
+            if(answer[1] > dist[summit]) {
+                answer[0] = summit;
+                answer[1] = dist[summit];
+            }
+        }
+        
+        return answer;
     }
 }
 
-class Path {
-    int num;
-    int weight;
-    
-    Path(int num, int weight) {
+class Node implements Comparable<Node>{
+    int num, weight;
+    Node(int num, int weight) {
         this.num = num;
         this.weight = weight;
-    }
-}
-
-class Node implements Comparable<Node> {
-    int num;
-    int intensity;
-    
-    Node(int num, int intensity) {
-        this.num = num;
-        this.intensity = intensity;
     }
     
     @Override
     public int compareTo(Node o) {
-        return intensity - o.intensity;
+        return this.weight - o.weight;
     }
 }
