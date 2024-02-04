b81b11ae695cbe1f9805bd50a444f8a974804d1b
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2024-02-05 01:16:44 +0900

    [level 4] Title: 단어 퍼즐, Time: 109.34 ms, Memory: 19.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**6)
+INF = int(1e9)
+
+def dfs(strs, t, idx, dp):
+    if idx == len(t):
+        return 0
+    
+    if dp[idx] != INF:
+        return dp[idx]
+    
+    dp[idx] = INF + 1
+        
+    for i in range(1, 6):
+        if idx + i > len(t):
+            continue;
+        
+        if t[idx:idx + i] in strs:
+            dp[idx] = min(dp[idx], dfs(strs, t, idx + i, dp) + 1)
+    
+    return dp[idx]
+            
+def solution(strs, t):
+    dp = [INF] * len(t)
+    answer = dfs(strs, t, 0, dp)
+    return -1 if answer == INF + 1 else answer
