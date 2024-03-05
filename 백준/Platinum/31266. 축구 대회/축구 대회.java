36ac2db064cc62c2bd88df5a205055dd08e4c370
Jungwoo Bae <51515170+wjddn2165@users.noreply.github.com>
2024-03-05 20:30:19 +0900

    [Platinum V] Title: 축구 대회, Time: 76 ms, Memory: 11628 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Platinum/31266. 축구 대회/축구 대회.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] score;
+    static long[][][] dp;
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        score = new int[N][4];
+        dp = new long[N][12][1 << 4];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                score[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= 11; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+
+        System.out.println(dfs(0, 0, 0));
+    }
+
+    static long dfs(int idx, int count, int bit) {
+        if (count == 11 && bit == (1 << 4) - 1) {
+            return 0;
+        }
+
+        if (idx == N) {
+            return 0;
+        }
+
+        if (dp[idx][count][bit] != -1) {
+            return dp[idx][count][bit];
+        }
+
+        dp[idx][count][bit] = dfs(idx + 1, count, bit);
+
+        for (int i = 0; i < 3; i++) {
+            dp[idx][count][bit] = Math.max(dp[idx][count][bit], dfs(
+                    idx + 1, count + 1, bit | (1 << i)
+            ) + score[idx][i]);
+        }
+
+        if (((1 << 3) & bit) == 0) {
+            dp[idx][count][bit] = Math.max(dp[idx][count][bit], dfs(
+                    idx + 1, count + 1, bit | (1 << 3)
+            ) + score[idx][3]);
+        }
+
+        return dp[idx][count][bit];
+    }
+}
